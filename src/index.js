4c51c2b8fea36f88fa91dab914e44a3a2245ac20
Jillian Burgess <jillian.burgess.7@gmail.com>
2020-09-20 22:16:01 -0400

    some old changes i never got around to pushing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+import 'dotenv/config';
+import cors from 'cors';
 import express from 'express'
 import models from './models'
 import routes from './routes'
 
 const app = express()
-const port = 7000
+const port = process.env.PORT || 7000
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
     req.context = {

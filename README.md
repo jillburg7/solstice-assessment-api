6241cdcd2606df0a1841375757c458e39a407102
Jillian Burgess <jillian.burgess.7@gmail.com>
2020-09-20 22:16:01 -0400

    some old changes i never got around to pushing

--- README.md
+++ README.md
@@ -29,5 +29,3 @@ Given this JSON file, you are asked to build a web app that should meet these re
 2. In a separate component: display in a table a list of all the accounts provided in the file.
 
 When a user clicks on a single customer on the table. the user is taken to a profile view containing information about that specific customer + a table with a list of all the accounts associated with the customer.
-
-Please send back your completed assessment by 6pm on Friday 6/26. You may send us a link to a zip containing your project or send it back to us by email.  A link to a GitHub repository would be preferred, as well as a live link to view the finished product.
--- src/routes/customers.js
+++ src/routes/customers.js
@@ -4,6 +4,22 @@ const router = Router()
 
 // Customer info endpoint
 router.get('/', (req, res) => res.send(req.context.models.customers))
+
+// Create new customer
+// test with:  curl -X POST -H "Content-Type:application/json" http://localhost:7000/customers -d '{"first_name":"Bob"}
+router.post('/', (req, res) => {
+    const id = 51;
+    const customer = {
+        id: id,
+        first_name: req.body.first_name,
+        active: 1,
+    }
+
+    req.context.models.customers[id] = customer
+    
+    return res.send(customer)
+})
+
 router.get('/:customerId', (req, res) => {
     const customer = req.context.models.customers.find(element => element.id === Number(req.params.customerId))
     return res.send(customer)
